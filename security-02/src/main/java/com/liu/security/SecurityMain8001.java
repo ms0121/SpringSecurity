4fd1b3a4639b6fed3aafe25cb553f75782039987
ms0121 <1132601565@qq.com>
2021-06-25 13:57:27 +0800

    1.0.2

---
+++ security-02/src/main/java/com/liu/security/SecurityMain8001.java
@@ -0,0 +1,16 @@
+package com.liu.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lms
+ * @date 2021-06-25 - 13:54
+ */
+
+@SpringBootApplication
+public class SecurityMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityMain8001.class, args);
+    }
+}
---
+++ security-02/src/main/java/com/liu/security/controller/SecurityController.java
@@ -0,0 +1,12 @@
+package com.liu.security.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author lms
+ * @date 2021-06-25 - 13:55
+ */
+
+public class SecurityController {
+
+}
