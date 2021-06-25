3426ad40226706e33f43c6cec30e18a0e2f5904f
ms0121 <1132601565@qq.com>
2021-06-25 14:12:50 +0800

    1.0.2

--- security-02/src/main/java/com/liu/security/SecurityMain8001.java
+++ security-02/src/main/java/com/liu/security/SecurityMain8001.java
@@ -2,13 +2,16 @@ package com.liu.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * @author lms
  * @date 2021-06-25 - 13:54
  */
 
-@SpringBootApplication
+// 排除security登录验证的功能
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@SpringBootApplication
 public class SecurityMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(SecurityMain8001.class, args);
--- security-02/src/main/java/com/liu/security/controller/SecurityController.java
+++ security-02/src/main/java/com/liu/security/controller/SecurityController.java
@@ -1,12 +1,23 @@
 package com.liu.security.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lms
  * @date 2021-06-25 - 13:55
  */
 
+@RestController
 public class SecurityController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("server/port")
+    public String getServerPort(){
+        return serverPort;
+    }
 
 }
