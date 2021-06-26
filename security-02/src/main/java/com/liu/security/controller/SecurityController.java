4e848da389221f19d28398208382258615f519d7
ms0121 <1132601565@qq.com>
2021-06-26 13:49:59 +0800

    1.0.2

--- security-02/src/main/java/com/liu/security/controller/SecurityController.java
+++ security-02/src/main/java/com/liu/security/controller/SecurityController.java
@@ -1,7 +1,6 @@
 package com.liu.security.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
