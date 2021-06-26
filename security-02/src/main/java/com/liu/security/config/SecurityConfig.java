308e69be084b276bfed577d9471dc064886f353e
ms0121 <1132601565@qq.com>
2021-06-26 08:25:33 +0800

    1.0.2

--- security-02/src/main/java/com/liu/security/config/SecurityConfig.java
+++ security-02/src/main/java/com/liu/security/config/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.liu.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
